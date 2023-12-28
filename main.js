1895b7bff2122f15760b43120e788b571097c424
jabezlam <108070688+jabezlam@users.noreply.github.com>
2023-12-28 20:26:52 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,500);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function draw() {
+    background('#C3C3C3');
+
+    document.getElementById("font_size").innerHTML="Font Size Of The Text Will Be:" + difference + "px";
+    textSize(difference);
+    fill('#FF2E2E');
+    text('jabez', 50, 400);
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX=" + leftWristX + "rightWristX=" + rightWristX + "difference=" + difference);
+    }
+}
